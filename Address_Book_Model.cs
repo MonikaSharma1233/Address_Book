2f0f5e7e1b362faee78911fc1f7a71a6e016df7b
firsttoken <monikasharma12330@gmail.com>
2023-02-03 10:25:05 +0530

    Model class of Addressbook

---
+++ Address_Book_Model.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Xml.Linq;
+
+namespace Address_Book_System
+{
+    internal class Address_Book_Model
+    {
+        internal string f_name;
+        internal string l_name;
+        internal string city;
+        internal string state;
+        internal double zip;
+        internal double phone;
+        internal string email;
+        //internal Address_Book_Model(string f_name, string l_name, string city, string state, string zip, string phone, string email)
+        //{
+        //    f_name=f_name;
+        //    l_name=l_name;
+        //    city=city;
+        //    state=state;
+        //    zip=zip;
+        //    phone=phone;
+        //    email=email;
+        //}
+    }
+    
+    
+}
+     
